c085e1c5febf882ecae3ef197014ade44ae4abf7
codemmk <2107228359@qq.com>
2024-12-17 22:09:33 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <main className="container">
-      <h1 className='text-sm'>Todo List</h1>
+      <h1 className='my-6 text-3xl font-bold text-center text-indigo-950'>TODO LIST</h1>
     </main>
   );
 }
