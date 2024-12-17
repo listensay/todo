b9c160e5e6fde4d1098c1d623013b17ae415dcae
codemmk <2107228359@qq.com>
2024-12-17 22:19:24 +0800

    双向绑定事件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import React, { useState } from "react";
+
 function App() {
+  const [inputValue, setInputValue] = useState("")
+
+  const onKeyUpHandle = (e: React.KeyboardEvent) => {
+    if(e.key === 'Enter') {
+      console.log(inputValue)
+    }
+  }
 
   return (
     <main className="container">
       <h1 className='my-6 text-3xl font-bold text-center text-indigo-950'>TODO LIST</h1>
+      <input type="text" value={inputValue} onChange={ e => setInputValue(e.target.value) } onKeyUp={ e => onKeyUpHandle(e) } />
     </main>
   );
 }
