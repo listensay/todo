81a52ae05e8323ecba3c0bce075ab1fa7521ab17
codemmk <2107228359@qq.com>
2024-12-19 20:40:54 +0800

    item config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss"
+import autoprefixer from "autoprefixer"
 import path from 'path';
 
 const host = process.env.TAURI_DEV_HOST;
@@ -7,6 +9,11 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [react()],
   clearScreen: false,
+  resolve:{
+    alias:{
+      '@':path.resolve(__dirname,'./src')
+    }
+  },
   server: {
     port: 1420,
     strictPort: true,
@@ -22,9 +29,12 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  css: {
+    postcss: {
+        plugins: [
+            tailwindcss,
+            autoprefixer,
+        ]
+    }
   },
 }));
